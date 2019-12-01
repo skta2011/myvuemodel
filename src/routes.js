62a9ec80261e2c9d330e7c0af8fe2bddfcd9f946
skta2011 <1091876738@qq.com>
2019-12-01 22:18:19 +0800

    edit 20191201

--- src/routes.js
+++ src/routes.js
@@ -1,87 +1,16 @@
-
+import Home from './views/test/Home.vue'
+import Container from './views/test/Container.vue'
 
 let routes = [
     {
-        path: '/login',
-        component: Login,
-        name: '',
-        hidden: true
-    },
-    {
-        path: '/404',
-        component: NotFound,
-        name: '',
-        hidden: true
-    },
-    //{ path: '/main', component: Main },
-    {
         path: '/',
         component: Home,
-        name: '',
-        iconCls: 'el-icon-menu',
-        leaf: true,//只有一个节点
-        children: [
-            { path: '/Index', component: Index, name: '首页' }
-        ]
+        name: 'mainpage'
     },
     {
-        path: '/',
-        component: Home,
-        name: '',
-        iconCls: 'el-icon-menu',//图标样式class
-        leaf: true,//只有一个节点
-        children: [
-            // { path: '/main', component: Main, name: '主页', hidden: true },
-            { path: '/Application', component: Application, name: '应用管理' },
-        ]
-    },
-    {
-        path: '/',
-        component: Home,
-        name: '日志管理',
-        iconCls: 'el-icon-menu',
-        children: [
-            { path: '/SysLog', component: SysLog, name: '系统日志' },
-            { path: '/AdminLog', component: AdminLog, name: '管理员操作日志' },
-            { path: '/UserLog', component: UserLog, name: '用户使用日志' }
-        ]
-    },
-    {
-        path: '/',
-        component: Home,
-        name: '配置管理',
-        iconCls: 'el-icon-menu',
-        children: [
-            { path: '/GuidePageConfig', component: GuidePageConfig, name: '引导页配置' },
-            { path: '/IntegralConfig', component: IntegralConfig, name: '积分规则配置' },
-            { path: '/SubscribeConfig', component: SubscribeConfig, name: '订阅栏目配置' },
-        ]
-    },
-    {
-        path: '/',
-        component: Home,
-        name: '权限管理',
-        iconCls: 'el-icon-menu',
-        children: [
-            { path: '/page4', component: Page4, name: '权限组管理' },
-            { path: '/Main', component: Main, name: '权限组权限管理' },
-            { path: '/Form', component: Form, name: '权限组人员管理' }
-        ]
-    },
-    {
-        path: '/',
-        component: Home,
-        name: '账号管理',
-        iconCls: 'el-icon-menu',
-        children: [
-            { path: '/user', component: user, name: '页面4' },
-            { path: '/page5', component: Page5, name: '页面5' }
-        ]
-    },
-    {
-        path: '*',
-        hidden: true,
-        redirect: { path: '/404' }
+        path: '/container',
+        component: Container,
+        name: 'container'
     }
 ];
 
---
+++ src/views/test/Container.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'mainpage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+</style>
