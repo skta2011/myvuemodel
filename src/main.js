c48763dfba8baa4f295e07affec06b87312ba175
skta2011 <1091876738@qq.com>
2019-12-09 22:55:43 +0800

    some change

--- src/main.js
+++ src/main.js
@@ -6,19 +6,41 @@ import ElementUI from 'element-ui'
 
 import App from './App'
 import routes from './routes'
-import Mock from './mock/mock'
 import 'element-ui/lib/theme-chalk/index.css'
 
+
+// 不使用mock时可以关闭
+import Mock from './mock/mock'
 Mock.init();
 
-Vue.config.productionTip = false
 
+
+
+/**
+ * 新增 init 方法：
+ * 1、初始化api的axio
+ */
+
+
+
+Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.use(VueRouter)
+
+
+
 
 const router = new VueRouter({
   routes
 })
+
+/**
+ * 新增store控制
+ */
+
+/**
+ * 新增router控制阀
+ */
 
 /* eslint-disable no-new */
 new Vue({
