019e4cf848fd698afd68ce39bae4832570835f26
skta2011 <1091876738@qq.com>
2019-12-10 20:53:14 +0800

    edit 3

---
+++ src/api/urls.js
@@ -0,0 +1,11 @@
+export const URL_QUERYUSER = '/queryUsers';
+
+export const URL_DELETEUSER = '/deleteUser';
+
+export const URL_EDITUSER = '/editUser';
+
+export default {
+    URL_QUERYUSER,
+    URL_DELETEUSER,
+    URL_EDITUSER
+}
